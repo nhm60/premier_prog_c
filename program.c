580e819d29312097d782e0670c73980e0836cded
nhm60 <noham-aissa.medjahed@esme.fr>
2025-09-19 08:46:52 +0000

    etape1+2

--- program.c
+++ program.c
@@ -2,24 +2,26 @@
 
 int main() {
 
-    printf"     _____________________________________________       "
-    printf" 130| 110   |                             | 2000  |2200  "
-    printf"    |__kts__|              |              |___m___|      "
-    printf" 120|                   ___|___                   |2100  "
-    printf"    |                  |       |                  |      "                                         
-    printf" 110|<         --------|   ¤   |--------         >|2000  "
-    printf"    |                  |___ ___|                  |      "
-    printf" 100|                      |                      |1900  "
-    printf"    |_____________________________________________|      "  
-    printf"; 90|                     180                     |1800  "
-    printf"    |                    ° ° °                    |      "
-    printf"  80|                  °        °                 |1700  "
-    printf"    |           270- °   __|__   ° -360           |      "
-    printf"    |                °     |     °                |1600      "
-    printf"    |                 °         °                 |      "
-    printf"    |                    ° ° °                    |      "                    
-    printf"    |                      90                     |      " 
-    printf"    |_____________________________________________|      "
+   int vitesse=110;
+   int altitude=2000;
+    printf("     _____________________________________________       \n");
+    printf(" %d| %d  |                              |  %d | %d    \n", vitesse+20,vitesse,altitude,altitude+200);
+    printf("    |__kts_|               |              |__ft___|      \n");
+    printf(" %d|                   ___|___                   | %d    \n", vitesse+10, altitude+100);
+    printf("    |                  |       |                  |      \n");                                         
+    printf(" %d|<         --------|   ¤   |--------         >| %d    \n", vitesse+0, altitude+0);
+    printf("    |                  |___ ___|                  |      \n");
+    printf(" %d|                      |                      | %d    \n", vitesse-10, altitude-100);
+    printf("    |_____________________________________________|      \n");  
+    printf(" %d |                     180                     | %d    \n", vitesse-20, altitude-200);
+    printf("    |                    ° ° °                    |      \n");
+    printf(" %d |                  °        °                 | %d    \n",vitesse-30, altitude-300);
+    printf("    |           270- °   __|__   ° -360           |      \n");
+    printf(" %d |                °     |     °                | %d    \n", vitesse-40, altitude-400);
+    printf("    |                 °         °                 |      \n");
+    printf("    |                    ° ° °                    |      \n");                    
+    printf("    |                      90                     |      \n"); 
+    printf("    |_____________________________________________|      \n");
    
                                      
     return 0;
